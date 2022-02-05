6f2f4d437397221b927c776c3f955b389f54ac4b
MarcusKD0 <92411837+MarcusKD0@users.noreply.github.com>
2022-02-04 21:18:58 -0500

    css and js update
    changed style for traffic nav and members text also added js to messages if fields are empty

--- js/message.js
+++ js/message.js
@@ -6,7 +6,26 @@ const search = document.getElementById('search');
 
 
 send.addEventListener('click', e =>{
-text.value = ''
-search.value = ''
-alert('Message Sent')
+
+if (text.value === '' && search.value === '') {
+  alert('Please fill out the empty fields');
+  search.style.borderColor = 'red'
+  text.style.borderColor = 'red'
+}
+  else if (search.value === '') {
+    alert('Please fill out the empty field');
+    search.style.borderColor = 'red'
+  }
+    else if (text.value==='') {
+      alert('Please fill out the empty field');
+      text.style.borderColor = 'red'
+}
+
+    else {
+      text.value = ''
+      text.style.borderColor = 'rgb(118, 118, 118)'
+      search.value = ''
+      search.style.borderColor = 'rgb(118, 118, 118)'
+      alert('Message Sent')
+}
 });
