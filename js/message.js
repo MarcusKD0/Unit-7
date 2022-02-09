11c398a47ecd0e76b21da362d3c2c17cb71f803c
MarcusKD0 <92411837+MarcusKD0@users.noreply.github.com>
2022-02-08 23:48:00 -0500

    style changes
    added some style and js changes

--- js/message.js
+++ js/message.js
@@ -9,23 +9,23 @@ send.addEventListener('click', e =>{
 
 if (text.value === '' && search.value === '') {
   alert('Please fill out the empty fields');
-  search.style.borderColor = 'red'
-  text.style.borderColor = 'red'
+  search.style.borderColor = 'red';
+  text.style.borderColor = 'red';
 }
   else if (search.value === '') {
-    alert('Please fill out the empty field');
-    search.style.borderColor = 'red'
+    alert('Please specify a user');
+    search.style.borderColor = 'red';
   }
     else if (text.value==='') {
-      alert('Please fill out the empty field');
-      text.style.borderColor = 'red'
+      alert('Please add a message');
+      text.style.borderColor = 'red';
 }
 
     else {
-      text.value = ''
-      text.style.borderColor = 'rgb(118, 118, 118)'
-      search.value = ''
-      search.style.borderColor = 'rgb(118, 118, 118)'
-      alert('Message Sent')
+      text.value = '';
+      text.style.borderColor = 'rgb(118, 118, 118)';
+      search.value = '';
+      search.style.borderColor = 'rgb(118, 118, 118)';
+      alert('Message Sent');
 }
 });
